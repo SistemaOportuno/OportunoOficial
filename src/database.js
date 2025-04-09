8c143a3d4841cbea4e0748840caadd50656e72eb
SistemaOportuno <oportuno2025@gmail.com>
2025-04-08 20:33:18 -0500

    Manejar errores de conexión a la base de datos y evitar crasheo

--- src/database.js
+++ src/database.js
@@ -1,27 +1,35 @@
-const mysql =require('mysql');
-const{promisify}=require('util');
-const {database}=require('./keys');
+const mysql = require('mysql');
+const { promisify } = require('util');
+const { database } = require('./keys');
 
-const pool= mysql.createPool(database);
+const pool = mysql.createPool(database);
 
-pool.getConnection((err,connection)=>{
-    if(err){
-        if(err.code==='PROTOCOL_CONNECTION_LOST'){
-            console.error('Se perdio la conexión de la Base de Datos');
-        }if(err.code==='ER_CON_COUNT_ERROR'){
-            console.error('Existen muchas conexiones');
-        }if(err.code==='ECONNREFUSED'){
-            console.error('Conexión rechazada');
+function handleDisconnect() {
+    pool.getConnection((err, connection) => {
+        if (err) {
+            console.error('Error al conectar a la base de datos:', err.message);
+            if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+                console.error('Se perdió la conexión de la Base de Datos');
+            } else if (err.code === 'ER_CON_COUNT_ERROR') {
+                console.error('Existen muchas conexiones');
+            } else if (err.code === 'ECONNREFUSED') {
+                console.error('Conexión rechazada');
+            } else if (err.code === 'PROTOCOL_SEQUENCE_TIMEOUT') {
+                console.error('Timeout en la conexión a la base de datos');
+            }
+            // Intentar reconectar después de 5 segundos
+            setTimeout(handleDisconnect, 5000);
+            return;
         }
-        return;
-    }
-    if(connection){
-        connection.release();
-        console.log('Conexión a la Base de Datos');
-        return;
-    }
-});
+        if (connection) {
+            connection.release();
+            console.log('Conexión a la Base de Datos');
+        }
+    });
+}
 
-pool.query=promisify(pool.query);
+handleDisconnect();
 
-module.exports=pool;
+pool.query = promisify(pool.query);
+
+module.exports = pool;
