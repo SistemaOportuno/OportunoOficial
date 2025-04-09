7d1028b59cb7f2d96f305fcc3f382873c622e9fb
SistemaOportuno <oportuno2025@gmail.com>
2025-04-08 20:44:45 -0500

    Añadir manejo de errores en index.js y ajustar database.js

--- src/database.js
+++ src/database.js
@@ -2,7 +2,14 @@ const mysql = require('mysql');
 const { promisify } = require('util');
 const { database } = require('./keys');
 
-const pool = mysql.createPool(database);
+const pool = mysql.createPool({
+    ...database,
+    connectTimeout: 60000, // 60 segundos
+    acquireTimeout: 60000, // 60 segundos para adquirir una conexión del pool
+    connectionLimit: 10, // Límite de conexiones en el pool
+    waitForConnections: true, // Esperar si no hay conexiones disponibles
+    queueLimit: 0 // Sin límite en la cola de conexiones
+});
 
 function handleDisconnect() {
     pool.getConnection((err, connection) => {
@@ -16,6 +23,8 @@ function handleDisconnect() {
                 console.error('Conexión rechazada');
             } else if (err.code === 'PROTOCOL_SEQUENCE_TIMEOUT') {
                 console.error('Timeout en la conexión a la base de datos');
+            } else if (err.code === 'ER_ACCESS_DENIED_ERROR') {
+                console.error('Acceso denegado: revisa las credenciales');
             }
             // Intentar reconectar después de 5 segundos
             setTimeout(handleDisconnect, 5000);
